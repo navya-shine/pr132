93ed09e0ba47b64dce1b3350c541a769ac2452b6
navya-shine <69377970+navya-shine@users.noreply.github.com>
2021-07-11 18:38:20 +0530

    Add files via upload

---
+++ Ac.js
@@ -0,0 +1,22 @@
+status = "";
+function preload()
+{
+    img = loadImage('Ac.jpg');
+}
+function setup()
+{
+    canvas = createCanvas(580, 360);
+    canvas.center();
+    objectDetecter = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Staus : Dectecting Objects";
+}
+function modelLoaded()
+{
+    console.log('Model Loaded');
+    status = true;
+    objectDetecter.detect(img , gotPoses);
+}
+function gotPoses()
+{
+    console.log(results);
+}
